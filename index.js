b50de5c0e25e5383fa10cf6c6f4aa93d4ab54da5
Pedro Camboim <pedrocamboim@klever.io>
2023-05-03 09:11:39 -0300

    Connect to database & config envs

--- index.js
+++ index.js
@@ -1,11 +1,32 @@
+require("dotenv").config();
 const express = require("express");
+
 const app = express();
-const port = 3000;
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
+const cookieParser = require("cookie-parser");
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
-});
+// database
+const connectDB = require("./db/connect");
+
+//  routers
+const authRouter = require("./routes/authRoutes");
+
+app.use(express.json());
+app.use(cookieParser(process.env.JWT_SECRET));
+
+app.use("/api/v1/auth", authRouter);
+
+const port = process.env.PORT || 5000;
+
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URL);
+    app.listen(port, () =>
+      console.log(`Server is listening on port ${port}...`)
+    );
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
