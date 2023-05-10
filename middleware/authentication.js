b6782d2d8d287d757f61c7fad5f6df9157340ee0
Pedro Camboim <pedrocamboim@klever.io>
2023-05-10 10:15:16 -0300

    handle expired token error

--- middleware/authentication.js
+++ middleware/authentication.js
@@ -28,6 +28,9 @@ const authenticateUser = async (req, res, next) => {
     if (error.name === "JsonWebTokenError") {
       return res.status(401).json({ error: "Invalid token" });
     }
+    if (error.name === "TokenExpiredError") {
+      return res.status(401).json({ error: "Token expired" });
+    }
     console.error(error);
     return res.status(500).json({ error: "Internal server error" });
   }
